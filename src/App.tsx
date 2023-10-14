3610f1b226f721fa19bad078c2d943e2ba10445d
Gabriel Boldi <gabriel.boldi@protonmail.com>
2023-10-14 14:56:43 -0300

    primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/Button/Button";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+
+      <Button variant="outline">123</Button>
+    </>
+  );
+}
+
+export default App;
