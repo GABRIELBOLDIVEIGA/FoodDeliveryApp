3f807adadd647bc4db25b9cc019d7541977429ff
Gabriel Boldi <gabriel.boldi@protonmail.com>
2023-11-16 20:29:09 -0300

    ...

--- src/lib/i18n/locales.ts
+++ src/lib/i18n/locales.ts
@@ -4,72 +4,72 @@ import themeEN from 'src/components/ToggleTheme/locale/en.json';
 import loginPT from 'src/pages/login/locale/pt.json';
 import loginEN from 'src/pages/login/locale/en.json';
 
-import bannerHomePT from 'src/pages/Client-User/Home/Banner/locale/pt.json';
-import bannerHomeEN from 'src/pages/Client-User/Home/Banner/locale/en.json';
+import bannerHomePT from '../../pages/Client-User/Home/Banner/locale/pt.json';
+import bannerHomeEN from '../../pages/Client-User/Home/Banner/locale/en.json';
 
-import categoriesHomePT from 'src/pages/Client-User/Home/Categories/locale/pt.json';
-import categoriesHomeEN from 'src/pages/Client-User/Home/Categories/locale/en.json';
+import categoriesHomePT from '../../pages/Client-User/Home/Categories/locale/pt.json';
+import categoriesHomeEN from '../../pages/Client-User/Home/Categories/locale/en.json';
 
-import dailyDealHomePT from 'src/pages/Client-User/Home/DailyDeal/locale/pt.json';
-import dailyDealHomeEN from 'src/pages/Client-User/Home/DailyDeal/locale/en.json';
+import dailyDealHomePT from '../../pages/Client-User/Home/DailyDeal/locale/pt.json';
+import dailyDealHomeEN from '../../pages/Client-User/Home/DailyDeal/locale/en.json';
 
-import homePT from 'src/pages/Client-User/Home/locale/pt.json';
-import homeEN from 'src/pages/Client-User/Home/locale/en.json';
+import homePT from '../../pages/Client-User/Home/locale/pt.json';
+import homeEN from '../../pages/Client-User/Home/locale/en.json';
 
-import categoriesPT from 'src/pages/Client-User/Categories/locale/pt.json';
-import categoriesEN from 'src/pages/Client-User/Categories/locale/en.json';
+import categoriesPT from '../../pages/Client-User/Categories/locale/pt.json';
+import categoriesEN from '../../pages/Client-User/Categories/locale/en.json';
 
-import productsAddPT from 'src/pages/Client-User/Products/locale/pt.json';
-import productsAddEN from 'src/pages/Client-User/Products/locale/en.json';
+import productsAddPT from '../../pages/Client-User/Products/locale/pt.json';
+import productsAddEN from '../../pages/Client-User/Products/locale/en.json';
 
-import cartEN from 'src/pages/Client-User/Cart/locale/en.json';
-import cartPT from 'src/pages/Client-User/Cart/locale/pt.json';
+import cartEN from '../../pages/Client-User/Cart/locale/en.json';
+import cartPT from '../../pages/Client-User/Cart/locale/pt.json';
 
-import profileEN from 'src/pages/Client-User/Profile/locale/en.json';
-import profilePT from 'src/pages/Client-User/Profile/locale/pt.json';
+import profileEN from '../../pages/Client-User/Profile/locale/en.json';
+import profilePT from '../../pages/Client-User/Profile/locale/pt.json';
 
-import ordersEN from 'src/pages/Client-User/Orders/locale/en.json';
-import ordersPT from 'src/pages/Client-User/Orders/locale/pt.json';
+import ordersEN from '../../pages/Client-User/Orders/locale/en.json';
+import ordersPT from '../../pages/Client-User/Orders/locale/pt.json';
 
 import menuEN from 'src/components/Menu/locale/en.json';
 import menuPT from 'src/components/Menu/locale/pt.json';
 
-import registerUserEN from 'src/pages/RegisterUser/locale/en.json';
-import registerUserPT from 'src/pages/RegisterUser/locale/pt.json';
+import registerUserEN from '../../pages/RegisterUser/locale/en.json';
+import registerUserPT from '../../pages/RegisterUser/locale/pt.json';
 
-import updateCategoryEN from 'src/pages/Adm-User/Categories/UpdateCategory/locale/en.json';
-import updateCategoryPT from 'src/pages/Adm-User/Categories/UpdateCategory/locale/pt.json';
+import updateCategoryEN from '../../pages/Adm-User/Categories/UpdateCategory/locale/en.json';
+import updateCategoryPT from '../../pages/Adm-User/Categories/UpdateCategory/locale/pt.json';
 
 import { EN, PT } from '../../pages/Adm-User/Categories/NewCategory/locale/locale'
 // import newCategoryPT from '../../pages/Adm-User/Categories/newCategory/locale/pt.json';
 // import newCategoryEN from '../../pages/Adm-User/Categories/NewCategory/locale/en.json';
 
-import productsAdmPT from 'src/pages/Adm-User/Products/locale/pt.json';
-import productsAdmEN from 'src/pages/Adm-User/Products/locale/en.json';
+import productsAdmPT from '../../pages/Adm-User/Products/locale/pt.json';
+import productsAdmEN from '../../pages/Adm-User/Products/locale/en.json';
 
-import newProductPT from 'src/pages/Adm-User/Products/NewProduct/locale/pt.json';
-import newProductEN from 'src/pages/Adm-User/Products/NewProduct/locale/en.json';
+import newProductPT from '../../pages/Adm-User/Products/NewProduct/locale/pt.json';
+import newProductEN from '../../pages/Adm-User/Products/NewProduct/locale/en.json';
 
-import updateProductPT from 'src/pages/Adm-User/Products/UpdateProduct/locale/pt.json';
-import updateProductEN from 'src/pages/Adm-User/Products/UpdateProduct/locale/en.json';
+import updateProductPT from '../../pages/Adm-User/Products/UpdateProduct/locale/pt.json';
+import updateProductEN from '../../pages/Adm-User/Products/UpdateProduct/locale/en.json';
 
-import ordersAdmPT from 'src/pages/Adm-User/Orders/locale/pt.json';
-import ordersAdmEN from 'src/pages/Adm-User/Orders/locale/en.json';
+import ordersAdmPT from '../../pages/Adm-User/Orders/locale/pt.json';
+import ordersAdmEN from '../../pages/Adm-User/Orders/locale/en.json';
 
-import ordersCardAdmPT from 'src/pages/Adm-User/Orders/OrderCardAdm/locale/pt.json';
-import ordersCardAdmEN from 'src/pages/Adm-User/Orders/OrderCardAdm/locale/en.json';
+import ordersCardAdmPT from '../../pages/Adm-User/Orders/OrderCardAdm/locale/pt.json';
+import ordersCardAdmEN from '../../pages/Adm-User/Orders/OrderCardAdm/locale/en.json';
 
-import sideDishAdmPT from 'src/pages/Adm-User/SideDish/locale/pt.json';
-import sideDishAdmEN from 'src/pages/Adm-User/SideDish/locale/en.json';
+import sideDishAdmPT from '../../pages/Adm-User/SideDish/locale/pt.json';
+import sideDishAdmEN from '../../pages/Adm-User/SideDish/locale/en.json';
 
-import usersPT from 'src/pages/Adm-User/Users/locale/pt.json';
-import usersEN from 'src/pages/Adm-User/Users/locale/en.json';
+import usersPT from '../../pages/Adm-User/Users/locale/pt.json';
+import usersEN from '../../pages/Adm-User/Users/locale/en.json';
 
-import cardUserPT from 'src/pages/Adm-User/Users/CardUser/locale/pt.json';
-import cardUserEN from 'src/pages/Adm-User/Users/CardUser/locale/en.json';
+import cardUserPT from '../../pages/Adm-User/Users/CardUser/locale/pt.json';
+import cardUserEN from '../../pages/Adm-User/Users/CardUser/locale/en.json';
 
-import cardUserInfosPT from 'src/pages/Adm-User/Users/InfosUser/locale/pt.json';
-import cardUserInfosEN from 'src/pages/Adm-User/Users/InfosUser/locale/en.json';
+import cardUserInfosPT from '../../pages/Adm-User/Users/InfosUser/locale/pt.json';
+import cardUserInfosEN from '../../pages/Adm-User/Users/InfosUser/locale/en.json';
 
 export const localeEN = {
   ...themeEN,
